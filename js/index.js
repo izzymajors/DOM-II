0d584df65c5c004ef735da939791479c986870b1
Jonathan-Majors <izzymajors@gmail.com>
2021-01-12 16:57:48 -0600

    added 2 listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+const navBar = document.querySelectorAll('nav a');
+    Array.from(navBar).forEach(elem => {
+        elem.addEventListener('click', function(event){
+            event.target.style.color = 'green';
+              //  event.stopPropagation();
+        });
+   
+ });
+
+ const imgOpacity = document.querySelectorAll('img');
+ imgOpacity.forEach(item => {
+    item.addEventListener('mousemove', () => {
+       item.style.opacity = '0.5';
+    })
+ })
